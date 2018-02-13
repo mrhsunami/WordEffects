3f15f1ff709c0cc0c88cebf8b70757eb3f9a4c47
Nathan <nathan.hsu@icloud.com>
2018-02-12 18:54:25 -0800

    Add ask for number in addition to string

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -10,8 +10,36 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // 255 unit long array of characters
+        while (1==1) {
+            char inputChars[255];
+            char inputNumber[255];
+            
+            printf("Input a string: ");
+            // limit input to max 255 characters
+            fgets(inputChars, 255, stdin);
+            
+            // print as a c string
+            printf("Your string is %s\n", inputChars);
+            
+            // convert char array to an NSString object
+            NSString *inputString = [NSString stringWithUTF8String:inputChars];
+            
+            // print NSString object
+            NSLog(@"Input was: %@", inputString);
+            
+            
+            
+            // asks for number
+            printf("Choose a number from 1 to 6: ");
+            // scans for input
+            fgets(inputNumber, 255, stdin);
+            // converts input to NSString for processing later
+            NSString *stringedNumber = [NSString stringWithUTF8String:inputNumber];
+            // logs out input * at this point user can put in not a number. Problem to be solved.
+            NSLog(@"Input number was: %@", stringedNumber);
+        }
+        
     }
     return 0;
 }
