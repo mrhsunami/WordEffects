b15a93a8527544d43103f77f7684642eac736629
Nathan <nathan.hsu@icloud.com>
2018-02-12 20:00:02 -0800

    Word_Effects/main.m

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -38,6 +38,68 @@ int main(int argc, const char * argv[]) {
             NSString *stringedNumber = [NSString stringWithUTF8String:inputNumber];
             // logs out input * at this point user can put in not a number. Problem to be solved.
             NSLog(@"Input number was: %@", stringedNumber);
+            
+            // convert input number to int
+            int numberChosen = [stringedNumber intValue];
+            
+            switch (numberChosen) {
+                case 1: {
+                    // 1. UPPERCASE
+                    NSString *upperCased = [inputString uppercaseString];
+                    NSLog(@"%@", upperCased);
+                    break;
+                }
+                case 2: {
+                    // 2. lowercase
+                    NSString *lowerCased = [inputString lowercaseString];
+                    NSLog(@"%@", lowerCased);
+                    break;
+                }
+                case 3: {
+                    // 3. Numberize // if possible, convert string to number *problem atm. will convert non-numbers to "0"
+                    int numberized = [inputString intValue];
+                    if (!numberized) {
+                        NSLog(@"That was not a valid number");
+                    } else {
+                        
+                        NSLog(@"%d", numberized);
+                    }
+                    break;
+                }
+                case 4: {
+                    // 4. Canadianize // append , eh? to it
+                    NSString *canadianized = [inputString stringByAppendingString:@", eh?"];
+                    NSLog(@"%@", canadianized);
+                    break;
+                }
+                case 5: {
+                    // 5. Respond // if ends with ?, say I don't know. If !, respond whoa, calm down!
+                    if ([inputString hasSuffix:@"?"]) {
+                        NSLog(@"I don't know");
+                    }
+                    else if ([inputString hasSuffix:@"!"]) {
+                        NSLog(@"Whoa, calm down!");
+                    } else {
+                        NSLog(@"No drama");
+                    }
+                    break;
+                }
+                case 6: {
+                    // 6. De-Space-It. // replace all space to ---
+                    NSString *dashDashDash = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+                    
+                    NSLog(@"%@", dashDashDash);
+                    break;
+                }
+                    
+                default:
+                    NSLog(@"Thats not a valid choice");
+                    break;
+            }
+            
+            
+            
+            
         }
         
     }
