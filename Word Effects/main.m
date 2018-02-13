ed233b689286936bdfad42cb47327e63bf925e31
Nathan <nathan.hsu@icloud.com>
2018-02-12 18:22:25 -0800

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Nathan Hsu on 2018-02-12.
+//  Copyright © 2018 Nathan Hsu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
